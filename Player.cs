d7afd1ad623d3a9e4bfa89fcadc646c2809e5e29
KadyVMorse <52608738+KadyVMorse@users.noreply.github.com>
2019-11-10 21:28:16 -0700

    Add files via upload
    Started working animations, colliders, and movement script.

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 50;
+    public float jumpPower = 150;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+
+
+  
+    void Start()
+    {
+        rb2d = GameObject GetComponet* Rigidbody2D(); 
+    }
+
+    void Update()
+    {
+
+
+        
+    }
+}
